5e0ce2035cb29367ed1c9779ef76109b73e9d091
Thabo Ntsoko <thabo.ginger@gmail.com>
2023-01-05 10:16:03 +0200

    Building league rankings

--- main/main.py
+++ main/main.py
@@ -1,5 +1,3 @@
-
-
 from collections import defaultdict
 from collections import namedtuple
 from typing import Any
@@ -23,6 +21,12 @@ def _individual_result(team_result: str) -> Team:
     return Team(team_name, points)
 
 
+def _rank_formatter(rank: int, name: str, points: int, carriage_return: bool) -> str:
+    pts = "pt" if points == 1 else "pts"
+    pts = f"{pts}\n" if carriage_return else pts
+    return f"{rank}. {name}, {points} {pts}"
+
+
 def match_result(game_result: Any) -> Tuple[Team, Team]:
     if game_result is None:
         raise ValueError("Invalid input. Value cannot be None")
@@ -37,13 +41,40 @@ def match_result(game_result: Any) -> Tuple[Team, Team]:
     return _individual_result(team_1), _individual_result(team_2)
 
 
-if __name__ == '__main__':
-    sample_input = "Lions 3, Snakes 3|Tarantulas 1, FC Awesome 0|Lions 1, FC Awesome 1|Tarantulas 3, Snakes 1|Lions 4, Grouches 0"
-    results = sample_input.split("|")
-    teams = defaultdict(int)
-    for result in results:
-        team_1, team_2 = match_result(result)
-        teams[team_1.name] += team_1.points
-        teams[team_2.name] += team_2.points
+def league_rankings(teams_with_points: dict) -> str:
+    if not teams_with_points:
+        raise ValueError("No teams with points found")
+    sorted_teams = sorted(teams_with_points.items(), key=lambda x: x[1], reverse=True)
+    num_teams_placed = len(sorted_teams)
+    rank = 1
+    prev = None
+    ranks = ""
+    for team in sorted_teams:
+        name, points = team
+        if prev is not None:
+            current_team_points = teams_with_points.get(name)
+            prev_team_points = teams_with_points.get(prev)
+            if current_team_points == prev_team_points:
+                rank -= 1
+        ranks += _rank_formatter(rank, name, points, num_teams_placed > 1)
+        rank += 1
+        prev = name
+        num_teams_placed -= 1
+    return ranks
 
-    print(teams)
+
+if __name__ == '__main__':
+    sample_input = "Lions 3, Snakes 0|Tarantulas 3, FC Awesome 0|Lions 1, FC Awesome 1|Tarantulas 3, Snakes 0|Lions 3, Grouches 0"
+    games = sample_input.split("|")
+    teams = defaultdict(int)
+    for game in games:
+        for team in match_result(game):
+            teams[team.name] += team.points
+    # 1. Lions, 7 pts
+    # 2. Tarantulas, 6 pts
+    # 3. FC Awesome, 1 pt
+    # 4. Grouches, 0 pts
+    # 4. Snakes, 0 pts
+    standings = league_rankings(teams)
+    print(standings)
+
--- tests/test_with_pytest.py
+++ tests/test_with_pytest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from main.main import league_rankings
 from main.main import match_result
 
 
@@ -21,3 +22,24 @@ def test_value_error_raised_when_input_format_is_invalid():
 def test_value_error_raised_when_input_format_is_invalid_for_individual_team_result():
     with pytest.raises(ValueError):
         match_result("Team1 3, Team2")
+
+
+def test_correct_match_result_returned():
+    team_1, team_2 = match_result("Team1 3, Team2 0")
+    assert team_1.name == "Team1"
+    assert team_2.name == "Team2"
+    assert team_1.points == 3
+    assert team_2.points == 0
+
+
+def test_value_error_raise_when_no_teams_with_points_exist():
+    with pytest.raises(ValueError):
+        league_rankings({})
+
+
+def test_correct_league_standings_returned():
+    teams = {'Team3': 7, 'Team1': 0, 'Team4': 6, 'Team2': 1, 'Team5': 0}
+    expected_standings = "1. Team3, 7 pts\n2. Team4, 6 pts\n" \
+                         "3. Team2, 1 pt\n4. Team1, 0 pts\n4. Team5, 0 pts"
+    actual_standings = league_rankings(teams)
+    assert expected_standings == actual_standings
